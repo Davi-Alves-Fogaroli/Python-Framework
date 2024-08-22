ee511f867d8076b72ecebf6b7151185a4363ae24
Davi <davi.alves.fogarolii@gmail.com>
2024-08-22 13:22:43 -0300

    new knoleges

--- Dash/dash_tutorial/graph_interact.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import pandas_datareader.data as web
-
-date_start = datetime.datetime(2015, 1, 1)
-date_end = datetime.datetime.now()
-
-df = web.DataReader("TLSA", "morningstar", date_start, date_end)
-df.set_index()
--- dash_tutorial/graph_interact.py
+++ dash_tutorial/graph_interact.py
@@ -1,15 +1,37 @@
-import datetime
-import pandas_datareader.data as web
+import pandas as pd
+from dash import html, dash, dcc, Input, Output
 
-date_start = datetime.datetime(2015, 1, 1)
-date_end = datetime.datetime.now()
+app = dash.Dash(__name__)
 
-df = web.DataReader("TLSA", "morningstar", date_start, date_end)
+app.layout = html.Div(children=[
+    html.H1(children="Go"),
+    dcc.Input(id="input", value="", type="text"),
+    html.Div(id="output-graph")
+])
 
-df.reset_index(inplace=True)
-df.set_index(inplace=True)
+@app.callback(
+    Output(component_id="output-graph", component_property="children"),
+    [Input(component_id="input", component_property="value")]   
+)
+def update_graph(input_data):
+    with open("data_file.json", "r") as file:
+        df = pd.read_json(file)
+        # print(df.head())
+        # time = df["time"].tolist()
+        # pressure = df["pressure"].tolist()
+        # app = dash.Dash()
+        df.set_index("time", inplace=True)
+    return dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': df.index, 'y': df, 'type': 'line', 'name': input_data},
+            ],
+            'layout': {
+                'title': input_data
+            }
+        }
+    )
 
-df = df.drop("Symbol", axis=1)
-
-print(df.head())
-
+if __name__ =="__main__":
+    app.run_server(debug=True)
